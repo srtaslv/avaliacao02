5a867d8966ce755fd3eb8d2305b4c9ed276197c7
Dayana Vitória <dayzzgwrl@gmail.com>
2026-06-16 19:31:18 -0300

    Adicionei a página do livro

--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def avaliacao(request):
+    if request.method == 'POST':
+
+        nome = request.POST.get('nome')
+        comentario = request.POST.get('comentario')
+        nota = request.POST.get('nota')
+
+        if (
+            len(nome) >= 3 and
+            len(comentario) >= 10 and
+            int(nota) >= 1 and
+            int(nota) <= 5
+        ):
+            return render(request, 'sucesso.html', {
+                'nome': nome,
+                'comentario': comentario,
+                'nota': nota
+            })
+
+    return render(request, 'index.html')
